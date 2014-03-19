ab5fe34645daf40c5dbff171db0ba4b265c64d60
liuda <liu.da101@gmail.com>
2014-03-19 22:47:19 +0800

    init the cesium and add drawLine method

---
+++ script/app.js
@@ -0,0 +1,33 @@
+(function(){
+  var cesiumWidget = new Cesium.CesiumWidget('cesiumContainer');
+  window.cesiumWidget = cesiumWidget;
+  CesiumAPI.setWidget(cesiumWidget);
+
+  var line = CesiumAPI.drawLine([
+    {longitude: -100.0, latitude:36.0, height:0},
+    {longitude: -92.0, latitude:36.0, height:300},
+    {longitude: -80.0, latitude:36.0, height:8890}
+    ], '1.0, 0.8, 0.1, 1.0', 2.0);
+
+  setTimeout(function(){
+    line.appearance = new Cesium.PolylineMaterialAppearance({
+        material : Cesium.Material.fromType('Color', {
+          color: new Cesium.Color(1.0, 1.0, 1.0, 1.0)
+        })
+      });
+  }, 3000);
+  
+
+  var operationDOM = $('#operationWrapper');
+  function showOperation(){
+    operationDOM.addClass('show');
+  }
+  function hideOperation(){
+    operationDOM.removeClass('show');
+  }
+
+
+  $('#tabs').tabs();
+  window.showOperation = showOperation;
+  window.hideOperation = hideOperation;
+})();
